ab22004d282a8ebbe5fd13ab1bc784399f33d4d9
theodorjohansson2@gmail.com <theodorjohansson2@gmail.com>
2026-04-01 18:51:15 +0200

    First try

--- main.c
+++ main.c
@@ -6,8 +6,29 @@ int main()
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
 
-   
+    int dice_rolls[100];
+    int roll_value = 0;
+    int sum = 0;
+    int throws = 0;
+    int score_list[6] = {0};
     // Your code goes here
+
+    for (int i = 0; i < 100; i++)
+    {
+        roll_value = (rand() % 6) + 1;
+        dice_rolls[i] = roll_value;
+        score_list[roll_value-1] += 1;
+        sum += roll_value;
+        throws += 1;
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d\n",score_list[i]);
+    }
+    printf("%d\n", sum);
+    printf("%.1f\n", (float)sum / throws);
+    
 
     return 0;
 }
